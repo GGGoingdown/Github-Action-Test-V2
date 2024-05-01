dc6902ccbcd9bcb92df39faf534e68bb8c90626c
GGGoingdown <eddiepai0401@gmail.com>
2024-05-01 14:54:31 +0800

    feat(api): add get users by id endpoint

--- src/main.py
+++ src/main.py
@@ -17,3 +17,8 @@ def read_item(item_id: int, q: str = None):
 @app.get("/users/me")
 def read_user_me():
     return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: str):
+    return {"user_id": user_id}
