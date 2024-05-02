be835167759d1ae778e1f4b9e342f48c3bdcc24a
GGGoingdown <eddiepai0401@gmail.com>
2024-05-02 20:45:23 +0800

    feat!: remove some apis

--- src/api.py
+++ src/api.py
@@ -37,22 +37,3 @@ def read_item_user(item_id: int, user_id: str, q: str = None):
 @router.get("/items/{item_id}/users/{user_id}/items/{item_id2}")
 def read_item_user_item(item_id: int, user_id: str, item_id2: int, q: str = None):
     return {"item_id": item_id, "user_id": user_id, "item_id2": item_id2, "q": q}
-
-
-@router.get("/items/{item_id}/users/{user_id}/items/{item_id2}/users/{user_id2}")
-def read_item_user_item_user(item_id: int, user_id: str, item_id2: int, user_id2: str, q: str = None):
-    return {"item_id": item_id, "user_id": user_id, "item_id2": item_id2, "user_id2": user_id2, "q": q}
-
-@router.get("/items/{item_id}/users/{user_id}/items/{item_id2}/users/{user_id2}/items/{item_id3}")
-def read_item_user_item_user_item(item_id: int, user_id: str, item_id2: int, user_id2: str, item_id3: int, q: str = None):
-    return {"item_id": item_id, "user_id": user_id, "item_id2": item_id2, "user_id2": user_id2, "item_id3": item_id3, "q": q}
-
-
-@router.get("/items/{item_id}/users/{user_id}/items/{item_id2}/users/{user_id2}/items/{item_id3}/users/{user_id3}")
-def read_item_user_item_user_item_users(item_id: int, user_id: str, item_id2: int, user_id2: str, item_id3: int, user_id3: str, q: str = None):
-    return {"item_id": item_id, "user_id": user_id, "item_id2": item_id2, "user_id2": user_id2, "item_id3": item_id3, "user_id3": user_id3, "q": q}
-
-
-@router.get("/items/{item_id}/users/{user_id}/items/{item_id2}/users/{user_id2}/items/{item_id3}/users/{user_id3}/items/{item_id4}")
-def read_item_user_item_user_item_users_item(item_id: int, user_id: str, item_id2: int, user_id2: str, item_id3: int, user_id3: str, item_id4: int, q: str = None):
-    return {"item_id": item_id, "user_id": user_id, "item_id2": item_id2, "user_id2": user_id2, "item_id3": item_id3, "user_id3": user_id3, "item_id4": item_id4, "q": q}
